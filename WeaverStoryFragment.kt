370611d6d3e0a64220c34c2aac023c6774c17fe4
Alina150704 <alinajohnvc@gmail.com>
2026-05-18 12:58:15 +0530

    Delete WeaverStoryFragment.kt

--- WeaverStoryFragment.kt
+++
@@ -1,9 +0,0 @@
-package com.example.nammavastra.model
-
-data class User(
-    val id: String = "",
-    val fullName: String = "",
-    val email: String = "",
-    val phone: String = "",
-    val password: String = ""
-)
