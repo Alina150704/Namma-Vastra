2cab0365b918de45e3b705a2f978d98f43ebb3de
Alina150704 <alinajohnvc@gmail.com>
2026-05-18 12:54:40 +0530

    Delete Color.kt

--- Color.kt
+++
@@ -1,8 +0,0 @@
-package com.example.nammavastra.model
-
-data class Message(
-    val id: String = "",
-    val text: String = "",
-    val isSentByUser: Boolean = false,  // true = user, false = seller
-    val timestamp: Long = System.currentTimeMillis()
-)
